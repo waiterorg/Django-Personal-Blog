ed891d8033bd32d359357f748b7b05139e1f5e24
waiterorg <konjkav79@gmail.com>
2021-02-17 14:21:09 +0330

    create first blog models-Article.

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
+
+
+
 
 # Create your models here.
+
+class Article(models.Model):
+    
+    STATUS_CHOICES = (
+        ('d','Draft'),
+        ('p','Published'),
+    )
+
+    title = models.CharField(max_length = 150, verbose_name='عنوان')
+    slug = models.SlugField(max_length= 150, unique = True , verbose_name='ادرس لینک')
+    description = models.TextField(verbose_name='توضیحات')
+    thumpnail = models.ImageField(upload_to = 'blog-images',verbose_name='تصویر')
+    publish = models.DateTimeField(default = timezone.now,verbose_name='منتشر شده')
+    created = models.DateTimeField(auto_now_add=True,verbose_name='ساخته شده')
+    updated = models.DateTimeField(auto_now=True,verbose_name='بروز رسانی شده')
+    status = models.CharField(max_length= 1,choices=STATUS_CHOICES,verbose_name='وضعیت')
+
+    def __str__(self):
+        return self.title
