e6aaa6f1a940d39f111126269b5e475da43bce68
waiterorg <konjkav79@gmail.com>
2021-02-17 15:34:00 +0330

    Set some filter and management for admin panel.

--- blog/blog/admin.py
+++ blog/blog/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Article
 # Register your models here.
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','created', 'updated', 'status')
+    list_filter = ('created', 'publish')
+    search_field = ('title', 'description')
+
+
+
+admin.site.register(Article, ArticleAdmin)
