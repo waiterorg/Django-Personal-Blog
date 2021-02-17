6c40eb49f865d249a21616e79de2e775a4fe2136
waiterorg <konjkav79@gmail.com>
2021-02-17 16:09:58 +0330

    fix article model.

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -16,7 +16,7 @@ class Article(models.Model):
     slug = models.SlugField(max_length= 150, unique = True , verbose_name='ادرس لینک')
     description = models.TextField(verbose_name='توضیحات')
     thumpnail = models.ImageField(upload_to = 'blog-images',verbose_name='تصویر')
-    published = models.DateTimeField(default = timezone.now,verbose_name='منتشر شده')
+    publish = models.DateTimeField(default = timezone.now,verbose_name='منتشر شده')
     created = models.DateTimeField(auto_now_add=True,verbose_name='ساخته شده')
     updated = models.DateTimeField(auto_now=True,verbose_name='بروز رسانی شده')
     status = models.CharField(max_length= 1,choices=STATUS_CHOICES,verbose_name='وضعیت')
