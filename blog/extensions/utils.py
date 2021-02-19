0a5b23540dbceee439aafe07a33483f3382e6992
waiterorg <konjkav79@gmail.com>
2021-02-19 22:03:33 +0330

    create persian number converter and use it.

--- blog/extensions/utils.py
+++ blog/extensions/utils.py
@@ -1,4 +1,26 @@
 from . import jalali
+from django.utils import timezone
+
+
+def persian_numbers_converter(mystr):
+    numbers = {
+        "0":"۰",
+        "1":"۱",
+        "2":"۲",
+        "3":"۳",
+        "4":"۴",
+        "5":"۵",
+        "6":"۶",
+        "7":"۷",
+        "8":"۸",
+        "9":"۹",
+    }
+    for e,p in numbers.items():
+        mystr = mystr.replace(e, p)
+
+    return mystr
+        
+
 
 
 def jalali_converter(time):
@@ -35,4 +57,4 @@ def jalali_converter(time):
         time.minute,
     )
 
-    return output
+    return persian_numbers_converter(output)
