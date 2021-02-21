46de7312db6a0396473abc38c28e69fa11bac019
waiterorg <konjkav79@gmail.com>
2021-02-21 14:27:12 +0330

    create instance for setting model.

--- blog/blog_setting/models.py
+++ blog/blog_setting/models.py
@@ -8,5 +8,9 @@ class settings(models.Model):
     site_copyright = models.CharField(max_length=120,verbose_name='قانون کپی رایت / صاحب سایت')
     short_description = models.CharField(max_length=150,verbose_name='معرفی سایت')
 
+    class Meta:
+        verbose_name = 'تنظیم سایت'
+        verbose_name_plural = 'تنظیمات سایت'
+    
     def __str__(self):
         return self.site_name
