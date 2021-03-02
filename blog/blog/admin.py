da5748f7288bb10158c9dc6b53c8b1a0f008bf12
waiterorg <konjkav79@gmail.com>
2021-03-02 19:50:23 +0330

    Create action in django.

--- blog/blog/admin.py
+++ blog/blog/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Article , Category
 # Register your models here.
+def make_published(self,request,queryset):
+    rows_updated = queryset.update(status='p')
+    if rows_updated == 1:
+        message_bit = 'منتشر شد'
+    else : 
+        message_bit = 'منتشر شدند'
+    self.message_user(request,'{} مقاله {}'.format(rows_updated, message_bit))
+
+make_published.short_description = 'انتشار مقالات انتخاب شده'
+
+
+def make_draft(self,request,queryset):
+    rows_updated = queryset.update(status='d')
+    if rows_updated == 1:
+        message_bit = 'پیش نویس شد'
+    else : 
+        message_bit = 'پیش نویس شدند'
+    self.message_user(request,'{} مقاله {}'.format(rows_updated, message_bit))
+
+make_draft.short_description = 'پیش نویس مقالات انتخاب شده'
+
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ( 'position', 'title', 'slug', 'parent', 'status',)
@@ -17,6 +39,7 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ('publish', 'status')
     search_field = ('title', 'description')
     ordering = ['-status','-publish']
+    actions = [make_draft, make_published]
 
     def category_to_str(self, obj):
         return " ,".join([category.title for category in obj.category.get_active_category()])
