1ac6521714ac96b81de6a6e35869f3e6e524f646
waiterorg <konjkav79@gmail.com>
2021-03-12 23:47:28 +0330

    Add login page and redirect page.

--- blog/account/views.py
+++ blog/account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from blog.models import Article
 
 # Create your views here.
+
+
+class ArticleList(LoginRequiredMixin, ListView):
+    queryset = Article.objects.all()
+    template_name = 'registration/home.html'
+
--- blog/config/urls.py
+++ blog/config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('', include('blog_setting.urls')),
+    path('account/',include('account.urls')),
 ]
 
 if settings.DEBUG:
