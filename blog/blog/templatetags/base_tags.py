7fe793a2a8f3781aebacd829990d8a3682a11f76
waiterorg <konjkav79@gmail.com>
2021-03-27 14:28:18 +0430

    Create base tag for active nav item and delete article in edit article for superuser.

--- blog/blog/templatetags/base_tags.py
+++ blog/blog/templatetags/base_tags.py
@@ -9,3 +9,12 @@ def category_navbar():
     return {
         "categories": Category.objects.get_active_category()
     }
+
+@register.inclusion_tag("registration/partials/link.html")
+def link(request, link_name, content):
+    return {
+        "request": request,
+        "link_name": link_name,
+        "link": "account:{}".format(link_name),
+        "content": content,
+    }
