193bbd6fcebb19ce76bae0e2cefd3375123d372b
waiterorg <konjkav79@gmail.com>
2021-03-29 17:13:33 +0430

    Edit profile success url and sidebar for users.

--- blog/account/urls.py
+++ blog/account/urls.py
@@ -1,16 +1,23 @@
 from django.contrib.auth import views
 from django.urls import path
-from .views import ArticleList, ArticleCreate, ArticleUpdate, ArticleDelete, Profile
+from .views import (
+    ArticleList,
+    ArticleCreate,
+    ArticleUpdate,
+    ArticleDelete, 
+    Profile,
+    Login
+)
 
 
-app_name='account'
+app_name = 'account'
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('login/', Login.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-#
+    #
     #path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
     #path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-#
+    #
     #path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
     #path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     #path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
@@ -18,7 +25,7 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path('', ArticleList.as_view(),name='home'),
+    path('', ArticleList.as_view(), name='home'),
     path('article/create', ArticleCreate.as_view(), name='article-create'),
     path('article/update/<int:pk>', ArticleUpdate.as_view(), name='article-update'),
     path('article/delete/<int:pk>', ArticleDelete.as_view(), name='article-delete'),
--- blog/account/views.py
+++ blog/account/views.py
@@ -2,52 +2,64 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from .models import User
 from .forms import ProfileForm
-from .mixins import FieldsMixin , FormValidMixin , AuthorAccessMixin, SuperUserAccessMixin
+from .mixins import FieldsMixin, FormValidMixin, AuthorAccessMixin, SuperUserAccessMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from blog.models import Article
 
 
 # Create your views here.
 class ArticleList(LoginRequiredMixin, ListView):
-	template_name = "registration/home.html"
+    template_name = "registration/home.html"
 
-	def get_queryset(self):
-		if self.request.user.is_superuser:
-			return Article.objects.all()
-		else:
-			return Article.objects.filter(author=self.request.user)
-
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Article.objects.all()
+        else:
+            return Article.objects.filter(author=self.request.user)
 
 
 class ArticleCreate(LoginRequiredMixin, FieldsMixin, FormValidMixin, CreateView):
-	model = Article
-	template_name = "registration/article-create-update.html"
+    model = Article
+    template_name = "registration/article-create-update.html"
+
 
 class ArticleUpdate(AuthorAccessMixin, FieldsMixin, FormValidMixin, UpdateView):
-	model = Article
-	template_name = "registration/article-create-update.html"
+    model = Article
+    template_name = "registration/article-create-update.html"
+
 
 class ArticleDelete(SuperUserAccessMixin, DeleteView):
-	model = Article
-	success_url = reverse_lazy('account:home')
-	template_name = "registration/article-confirm-delete.html"
+    model = Article
+    success_url = reverse_lazy('account:home')
+    template_name = "registration/article-confirm-delete.html"
 
 
-class Profile(UpdateView):
-	model = User
-	template_name = "registration/profile.html"
-	form_class = ProfileForm
+class Profile(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = "registration/profile.html"
+    form_class = ProfileForm
 
-	success_url = reverse_lazy("account:profile")
+    success_url = reverse_lazy("account:profile")
 
-	def get_object(self):
-	 return User.objects.get(pk = self.request.user.pk)
+    def get_object(self):
+        return User.objects.get(pk=self.request.user.pk)
 
-	def get_form_kwargs(self):
-		kwargs = super(Profile,self).get_form_kwargs()
-		kwargs.update({
-			'user': self.request.user
-		})
-		return kwargs
+    def get_form_kwargs(self):
+        kwargs = super(Profile, self).get_form_kwargs()
+        kwargs.update({
+            'user': self.request.user
+        })
+        return kwargs
+
+
+class Login(LoginView):
+    def get_success_url(self):
+        user = self.request.user
+
+        if user.is_superuser or user.is_author:
+            return reverse_lazy("account:home")
+        else:
+            return reverse_lazy("account:profile")
