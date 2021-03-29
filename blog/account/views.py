f7579f87a4db597e5dde7c14241fb72ca1404ce7
waiterorg <konjkav79@gmail.com>
2021-03-29 16:29:49 +0430

    Create profile form.

--- blog/account/views.py
+++ blog/account/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import User
+from .forms import ProfileForm
 from .mixins import FieldsMixin , FormValidMixin , AuthorAccessMixin, SuperUserAccessMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from blog.models import Article
@@ -37,9 +38,16 @@ class ArticleDelete(SuperUserAccessMixin, DeleteView):
 class Profile(UpdateView):
 	model = User
 	template_name = "registration/profile.html"
-	fields = ['username', 'email', 'first_name', 'last_name', 'special_user', 'is_author']
+	form_class = ProfileForm
 
 	success_url = reverse_lazy("account:profile")
 
 	def get_object(self):
 	 return User.objects.get(pk = self.request.user.pk)
+
+	def get_form_kwargs(self):
+		kwargs = super(Profile,self).get_form_kwargs()
+		kwargs.update({
+			'user': self.request.user
+		})
+		return kwargs
