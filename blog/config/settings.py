c077e1f77330aed6f631228703ec71584fee25f2
waiterorg <konjkav79@gmail.com>
2021-04-10 14:43:50 +0430

    Add gravatar to admin panel.

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'crispy_forms',
     'six',
+    'django_gravatar',
     # apps
     'blog.apps.BlogConfig',
     'blog_setting.apps.BlogSettingConfig',
