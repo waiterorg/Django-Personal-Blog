b16ae51f1b9fdb79a6ad07db84f218a0552cadb5
waiterorg <konjkav79@gmail.com>
2021-05-01 18:48:04 +0430

    humanize article.

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     #added
     'extensions',
     'django_render_partial',
